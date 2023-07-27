ac57d1c9ebda621147fab7177fe53f11c9062de5
Harsha Padidala <abhi@Abhisheks-MBP.attlocal.net>
2023-07-27 15:39:29 -0500

    Initialize movie catalog service

---
+++ movie-catalog-service/src/main/java/com/springcraft/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,25 @@
+package com.springcraft.moviecatalogservice.resources;
+
+import com.springcraft.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+        return Collections.singletonList(
+                new CatalogItem("Catch me if you can", "Real", 4)
+        );
+    }
+}
